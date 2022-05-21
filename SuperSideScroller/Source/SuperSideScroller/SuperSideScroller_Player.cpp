cff3018f6d98cbb6ace2022a7b68a13518e59ba2
jaewook <3355moijam@gmail.com>
2022-05-21 17:31:52 +0900

    character animation

---
+++ SuperSideScroller/Source/SuperSideScroller/SuperSideScroller_Player.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SuperSideScroller_Player.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+ASuperSideScroller_Player::ASuperSideScroller_Player()
+{
+	bIsSprinting = false;
+	GetCharacterMovement()->MaxWalkSpeed = 300.0f;
+}
+
+void ASuperSideScroller_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &ASuperSideScroller_Player::Sprint);
+	PlayerInputComponent->BindAction("Sprint", IE_Released, this, &ASuperSideScroller_Player::StopSprinting);
+}
+
+void ASuperSideScroller_Player::Sprint()
+{
+	if (!bIsSprinting)
+	{
+		bIsSprinting = true;
+		GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+	}
+}
+
+void ASuperSideScroller_Player::StopSprinting()
+{
+	if (bIsSprinting)
+	{
+		bIsSprinting = false;
+		GetCharacterMovement()->MaxWalkSpeed = 300.0f;
+	}
+}
